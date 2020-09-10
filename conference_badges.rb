173ced3a79050f0a61f59fac570efad0e485ea23
CJ Davis <csdj92@gmail.com>
2020-09-10 12:38:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-names=[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 def badge_maker(names)
   puts "Hello, my name is #{names}."
   
