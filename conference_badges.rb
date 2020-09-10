fc5c954cb8da4265b25369ca40d2ecbe5e036e68
CJ Davis <csdj92@gmail.com>
2020-09-10 12:43:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(attendees)
   puts "Hello, my name is #{attendees}."
   
   
-end
+endattendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
